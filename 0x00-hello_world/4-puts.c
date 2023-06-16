00b198042eea28da56b1aa0a036f041b2e5cc8d5
MAHMOUDMOD <mahmoud.mmlatif@gmail.com>
2023-06-15 17:42:59 -0700

    2>4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
-int main() {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+/**
+*main - entry poit
+*return: always 0 (success)
+*/ int main(void)
+{
+          puts ("\"Programming is like building a multilingual puzzle");
+          return (o);
 }
